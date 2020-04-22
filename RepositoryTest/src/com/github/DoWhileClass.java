19597614e4616f44b9af558030f0d8916d34acf3
karthikdarun <61750253+karthikdarun@users.noreply.github.com>
2020-04-22 19:57:29 +0530

    Do while pgm

---
+++ RepositoryTest/src/com/github/DoWhileClass.java
@@ -0,0 +1,14 @@
+package com.github;
+
+public class DoWhileClass {
+
+	public static void main(String[] args) {
+		
+		int i = 100;
+		do {
+		 System.out.println(i);
+		 i= i-10;
+		}while (i>=10);
+		
+	}
+}
